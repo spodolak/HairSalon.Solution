c929b0b9dbce5a4244427943a18d4065c6a22e71
Stephanie Podolak <StephPodolak@gmail.com>
2020-03-20 16:43:37 -0700

    add controller and view logic to allow user to add new client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -17,5 +17,22 @@ namespace HairSalon.Controllers
     {
       _db = db;
     }
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create (Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
